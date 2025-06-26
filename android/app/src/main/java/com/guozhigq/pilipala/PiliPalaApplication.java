f2813b9dddcddd0a40e8cdba5b2ac5927c8d7f1f
xiaoka <13716875238@163.com>
2025-06-26 14:45:39 +0800

    build(android): 修复 Android 应用的 Multidex 问题- 在 build.gradle 中添加了 androidx.multidex:multidex依赖 - 创建了 PiliPalaApplication 类，继承自 MultiDexApplication - 通过这些修改，解决了应用在 Android 4.0 及以下版本上的运行问题

---
+++ android/app/src/main/java/com/guozhigq/pilipala/PiliPalaApplication.java
@@ -0,0 +1,10 @@
+package com.guozhigq.pilipala;
+
+import androidx.multidex.MultiDexApplication;
+
+public class PiliPalaApplication extends MultiDexApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
